58b00d88aa04f490cd4dfdb73b304df9112f738d
Neha <nn0933@princeton.edu>
2023-07-05 16:56:27 -0400

    init commit

---
+++ snr/xray.py
@@ -0,0 +1,17 @@
+import numpy as np
+from matplotlib import pyplot as plt
+'''import yt
+import pyxsim
+import soxs
+import moviepy
+import moviepy.video.io.ImageSequenceClip
+from moviepy.editor import *'''
+
+import sys
+sys.path.append('/home/nn0933/athenaresearch/python/') # path to athena_data and athena_kit module
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["mathtext.fontset"] = "dejavuserif"
+plt.rcParams["figure.facecolor"] = "white"
+from athena_data_2023_01 import AthenaBinary
+from athena_data_2023_01 import AthenaBinaries
+import athena_kit as ak
